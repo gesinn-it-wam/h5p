f4ff14dd46e14b4800f047e247b7aa8dddb640dd
Markus <92986684+gesinn-it-wam@users.noreply.github.com>
2023-05-30 17:49:59 +0200

    Drupal 9

--- context/install-h5p.sh
+++ context/install-h5p.sh
@@ -5,7 +5,7 @@ sleep 10
 
 drush site:install --db-url=mysql://drupal:drupal@mariadb/drupal --account-pass=drupal4everyone -y
 # save /var/www/html/web/sites/default/settings.php to a mounted /data (like in DSC)?
-composer require 'drupal/h5p:^1.51'
+composer require 'drupal/h5p:^2.0.0-alpha3'
 drush en h5p h5peditor --y
 drush user:create embassy --password='h5ptest' -y
 
@@ -14,11 +14,11 @@ composer require drupal/zen:6.4.0
 drush en -y zen
 drush vset theme_default zen
 
-# add extra.css to zen theme
-ZEN=/var/www/html/web/sites/all/themes/contrib/zen
-# linking seems not to work here
-# ln -s /extras/extra.css $ZEN/extra.css
-cp /extras/extra.css $ZEN/extra.css
-echo "stylesheets[all][] = extra.css" >> $ZEN/zen.info
+# install mw extension
+cp /extras/mw /var/ww/html/web/sites/all/modules/contrib
+drush en mw --y
+
+cp /extras/mw/extra.css /var/ww/html/web/sites/all/modules/contrib/h5p
+echo ""
 
 drush cache-clear all
